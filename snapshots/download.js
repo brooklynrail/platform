0acefce79f8ad2c90fd9bea5fa36e8d269ad428e
jeremyzilar <jeremyzilar@gmail.com>
2023-11-02 14:58:35 -0400

    added node script for downloading a config snapshot

---
+++ snapshots/download.js
@@ -0,0 +1,17 @@
+const https = require("https");
+const fs = require("fs");
+require('dotenv').config();
+
+const token = process.env.TOKEN;
+const url = "https://studio.brooklynrail.org/schema/snapshot?export=yaml&access_token=" + token;
+
+https.get(url, (res) => {
+  const path = "snapshots/init.yaml";
+  const writeStream = fs.createWriteStream(path);
+  res.pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    writeStream.close();
+    console.log("💥 Snapshot has been downloaded!");
+  });
+});
