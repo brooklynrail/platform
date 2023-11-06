49efa30471b4e8a761ed548c5807feff667c7e9c
jeremyzilar <jeremyzilar@gmail.com>
2023-11-06 15:49:59 -0500

    add node snapshot

---
+++ snapshots/sync-from/prod.js
@@ -0,0 +1,49 @@
+const fetch = require('cross-fetch');
+require('dotenv').config();
+const token = process.env.TOKEN;
+
+const BASE_DIRECTUS_URL = 'https://studio.brooklynrail.org';
+const BASE_ACCESS_TOKEN = process.env.TOKEN;
+
+const TARGET_DIRECTUS_URL = 'http://127.0.0.1:8055';
+const TARGET_ACCESS_TOKEN = 'oK3jgAPaK7Hg-_eUxg-wNajajTvMPmcn';
+
+async function main() {
+	const snapshot = await getSnapshot();
+	const diff = await getDiff(snapshot);
+	await applyDiff(diff);
+}
+
+main();
+
+async function getSnapshot() {
+	const URL = `${BASE_DIRECTUS_URL}/schema/snapshot?access_token=${BASE_ACCESS_TOKEN}`;
+	const { data } = await fetch(URL).then((r) => r.json());
+	return data;
+}
+
+async function getDiff(snapshot) {
+	const URL = `${TARGET_DIRECTUS_URL}/schema/diff?access_token=${TARGET_ACCESS_TOKEN}`;
+
+	const { data } = await fetch(URL, {
+		method: 'POST',
+		body: JSON.stringify(snapshot),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((r) => r.json());
+
+	return data;
+}
+
+async function applyDiff(diff) {
+	const URL = `${TARGET_DIRECTUS_URL}/schema/apply?access_token=${TARGET_ACCESS_TOKEN}`;
+
+	await fetch(URL, {
+		method: 'POST',
+		body: JSON.stringify(diff),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+}
