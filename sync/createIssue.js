10eb79a7b0f3b4e4f1f30818be527ec9548b1ff0
jeremyzilar <jeremyzilar@gmail.com>
2023-12-11 10:52:49 -0500

    update createIssue to focus on a single issue

--- sync/createIssue.js
+++ sync/createIssue.js
@@ -1,5 +1,5 @@
-const fetch = require('isomorphic-fetch');
-const { createDirectus, rest, withToken, createItems, readItems, readActivity} = require('@directus/sdk');
+require('dotenv').config();
+const { createDirectus, rest, withToken, createItems, importFile, readItems, readActivity} = require('@directus/sdk');
 
 const BASE_DIRECTUS_URL = 'http://127.0.0.1:8055';
 const BASE_ACCESS_TOKEN = process.env.TOKENLOCAL;
@@ -8,7 +8,7 @@ const API_ENDPOINT = 'http://localhost:8000';
 async function isSection(old_section_id, client) {
   try {
     const section = await client.request(
-      withToken(BASE_ACCESS_TOKEN, readItems('section', {
+      withToken(BASE_ACCESS_TOKEN, readItems('Sections', {
         "filter": {
           "old_id": {
             "_eq": old_section_id
@@ -34,7 +34,7 @@ async function isPeople(peopleIds, client) {
     // Function to check if a person with a given ID exists in Directus
     const checkPersonExists = async (personId, client) => {
       const person = await client.request(
-        withToken(BASE_ACCESS_TOKEN, readItems('people', {
+        withToken(BASE_ACCESS_TOKEN, readItems('People', {
           "filter": {
             "old_id": {
               "_eq": personId
@@ -50,14 +50,15 @@ async function isPeople(peopleIds, client) {
     for (const personId of peopleIds) {
       const existingPersonId = await checkPersonExists(personId, client);
       if (existingPersonId) {
-        existingPeople.push({ people_id: existingPersonId });
-      }
+        existingPeople.push({ People_id: existingPersonId });
+      } 
     }
 
     if(existingPeople.length == 0){
       console.log('Error fetching existingPeople');
       return;
     }
+    
     return existingPeople;
    
   } catch (error){
@@ -65,59 +66,84 @@ async function isPeople(peopleIds, client) {
   }
 }
 
+
+// ============
+
+async function importFeaturedImage(data, client) {
+  try {
+    if(data){
+      const path = `https://storage.googleapis.com/rail-legacy-media/production${data.path}`;
+      console.log(path);
+      const description = data.description;
+
+      const result = await client.request(
+        importFile(path, {
+          description: description || null,
+        })
+      );
+      console.log("featured_image ------>");
+      console.log(result);
+      // return the ID of the file that was just uploaded
+      return result.id;
+    }
+  } catch (error){
+    console.error('Error fetching Featured Image:', error.message);
+  }
+}
+
+
 // ============
 
 async function importIssues(issues) {
   const client = createDirectus(BASE_DIRECTUS_URL).with(rest());
   try {
-    // Fetch the list of issues
-    const issues = await fetchIssues();
+    
     // Iterate over each issue
-    for (const issue of issues) {
-      const issueUrl = `${API_ENDPOINT}/${issue.year}/${issue.month}/api`;
+    
+    const issueUrl = `${API_ENDPOINT}/2023/11/api`;
 
-      console.log("------------------- - - - -")
-      console.log(`Importing issue for ${issue.year}-${issue.month}`);
+    console.log("------------------- - - - -")
+    console.log(`Importing issue`);
 
-      // Fetch data for the current issue
-      const response = await fetch(issueUrl, {
-        headers: {
-          Authorization: `Bearer ${BASE_ACCESS_TOKEN}`,
-        },
-      });
+    // Fetch data for the current issue
+    const response = await fetch(issueUrl, {
+      headers: {
+        Authorization: `Bearer ${BASE_ACCESS_TOKEN}`,
+      },
+    });
 
-      if (!response.ok) {
-        console.error(`Error fetching data for issue ${issue.year}/${issue.month}: HTTP error! Status: ${response.status}`);
-        continue; // Skip to the next issue
-      }
-
-      const data = await response.json();
-      
-      const articles = await Promise.all(data.article.map(async (article) => {
-        const section = await isSection(article.article_id.old_section_id, client);
-        const people = await isPeople(article.article_id.people, client);
-        return {
-          ...article,
-          article_id: {
-            ...article.article_id,
-            section,
-            people,
-          },
-        };
-      }));
-      
-      const newData = await {
-        ...data,
-        article: articles,
-      };
-
-      const createIssue = await client.request(
-        withToken(BASE_ACCESS_TOKEN, createItems('issue', newData))
-      );
-      
-      console.log(`Import for ${issue.year}-${issue.month} completed successfully`);
-      console.log(createIssue);
+    if (!response.ok) {
+      console.error(`Error fetching data for issue ${issue.year}/${issue.month}: HTTP error! Status: ${response.status}`);
     }
+
+    const data = await response.json();
+    
+    const articles = await Promise.all(data.articles.map(async (article) => {
+      const section = await isSection(article.Articles_id.old_section_id, client);
+      const people = await isPeople(article.Articles_id.people, client);
+      const featured_image = await importFeaturedImage(article.Articles_id.featured_image, client);
+      return {
+        ...article,
+        Articles_id: {
+          ...article.Articles_id,
+          section,
+          people,
+          featured_image,
+        },
+      };
+    }));
+    
+    const newData = await {
+      ...data,
+      articles: articles,
+    };
+
+    const createIssue = await client.request(
+      withToken(BASE_ACCESS_TOKEN, createItems('Issues', newData))
+    );
+    
+    console.log(createIssue);
+    
     console.log('Full import completed successfully.');
 
   } catch (error) {
@@ -126,22 +152,6 @@ async function importIssues(issues) {
   }
 }
 
-async function fetchIssues() {
-  try {
-    // Fetch issues from your API
-    const response = await fetch(`${API_ENDPOINT}/api/issue-list`);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch issues. Status: ${response.status}`);
-    }
-
-    // Parse the JSON response
-    const issues = await response.json();
-    return issues;
-  } catch (error) {
-    console.error('Error fetching issues:', error.message);
-    throw error; // Propagate the error
-  }
-}
 
 // Start the import process
 importIssues();
