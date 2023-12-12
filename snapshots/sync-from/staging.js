d17acba5aad11bf95529249ba2ed8b4f9faad419
jeremyzilar <jeremyzilar@gmail.com>
2023-12-11 19:30:42 -0500

    add staging sync file

---
+++ snapshots/sync-from/staging.js
@@ -0,0 +1,48 @@
+const fetch = require('cross-fetch');
+require('dotenv').config();
+
+const BASE_DIRECTUS_URL = 'https://brooklynrail-studio-staging-jy3zptd2sa-wl.a.run.app/';
+const BASE_ACCESS_TOKEN = process.env.TOKEN_STAGING;
+
+const TARGET_DIRECTUS_URL = 'http://127.0.0.1:8055';
+const TARGET_ACCESS_TOKEN = process.env.TOKEN_LOCAL;
+
+async function main() {
+	const snapshot = await getSnapshot();
+	const diff = await getDiff(snapshot);
+	await applyDiff(diff);
+}
+
+main();
+
+async function getSnapshot() {
+	const URL = `${BASE_DIRECTUS_URL}/schema/snapshot?access_token=${BASE_ACCESS_TOKEN}`;
+	const { data } = await fetch(URL).then((r) => r.json());
+	return data;
+}
+
+async function getDiff(snapshot) {
+	const URL = `${TARGET_DIRECTUS_URL}/schema/diff?access_token=${TARGET_ACCESS_TOKEN}`;
+
+	const { data } = await fetch(URL, {
+		method: 'POST',
+		body: JSON.stringify(snapshot),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((r) => r.json());
+
+	return data;
+}
+
+async function applyDiff(diff) {
+	const URL = `${TARGET_DIRECTUS_URL}/schema/apply?access_token=${TARGET_ACCESS_TOKEN}`;
+
+	await fetch(URL, {
+		method: 'POST',
+		body: JSON.stringify(diff),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+}
