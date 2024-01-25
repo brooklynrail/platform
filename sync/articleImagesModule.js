20998fbd6277e586006b90444ee0e8a4ac6fbbd6
jeremyzilar <jeremyzilar@gmail.com>
2024-01-25 11:43:54 -0500

    add module for handling article images

---
+++ sync/articleImagesModule.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+require("dotenv").config();
+const { importImageModule } = require("./importImageModule");
+
+async function articleImagesModule(articleImagesIds, client) {
+  try {
+    const images = [];
+
+    // Iterate over each personId and check if it exists in Directus
+    for (const articleImageData of articleImagesIds) {
+      const imageId = await importImageModule(articleImageData, client);
+      if (imageId) {
+        images.push({ directus_files_id: imageId });
+      }
+    }
+
+    if (images.length != 0) {
+      return images;
+    }
+  } catch (error) {
+    console.error("Error uploading image:", error.message);
+
+    // Handle the error and write specific data to a text file
+    const failedData = `${articleImagesIds}\n`;
+    const filePath = `sync/errors-images.txt`;
+
+    // Write the error data to the text file
+    fs.writeFileSync(filePath, failedData, "utf-8");
+  }
+}
+
+module.exports = {
+  articleImagesModule,
+};
--- sync/issueModule.js
+++ sync/issueModule.js
@@ -1,10 +1,16 @@
-const fs = require('fs');
-require('dotenv').config();
-const { BASE_ACCESS_TOKEN, BASE_DIRECTUS_URL } = require('./config');
-const { importImageModule } = require('./importImageModule');
-const { sectionsModule } = require('./sectionsModule');
-const { contributorsModule } = require('./contributorsModule');
-const { createDirectus, rest, withToken, createItems} = require('@directus/sdk');
+const fs = require("fs");
+require("dotenv").config();
+const { BASE_ACCESS_TOKEN, BASE_DIRECTUS_URL } = require("./config");
+const { importImageModule } = require("./importImageModule");
+const { sectionsModule } = require("./sectionsModule");
+const { contributorsModule } = require("./contributorsModule");
+const { articleImagesModule } = require("./articleImagesModule");
+const {
+  createDirectus,
+  rest,
+  withToken,
+  createItems,
+} = require("@directus/sdk");
 
 // ============
 
@@ -22,16 +28,36 @@ async function importCoverImages(data, client) {
 async function importArticles(data, client) {
   return Promise.all(
     data.articles.map(async (article) => {
-      const sections = await sectionsModule(article.articles_id.old_section_id, client);
-      const contributors = await contributorsModule(article.articles_id.contributors, client);
-      const featured_image = await importImageModule(article.articles_id.featured_image, client);
+      const sections = await sectionsModule(
+        article.articles_slug.old_section_id,
+        client
+      );
+      const contributors = await contributorsModule(
+        article.articles_slug.contributors,
+        client
+      );
+
+      const featured_image = await importImageModule(
+        article.articles_slug.featured_image,
+        client
+      );
+
+      const images = await articleImagesModule(
+        article.articles_slug.images,
+        client
+      );
+
+      console.log("Images -----");
+      console.log(images);
+
       return {
         ...article,
-        articles_id: {
-          ...article.articles_id,
+        articles_slug: {
+          ...article.articles_slug,
           sections,
           contributors,
           featured_image,
+          images,
         },
       };
     })
@@ -42,7 +68,7 @@ async function importIssue(data) {
   const client = createDirectus(BASE_DIRECTUS_URL).with(rest());
   try {
     if (data) {
-      console.log('+++++++++++++++++++++++++++++++');
+      console.log("+++++++++++++++++++++++++++++++");
       console.log(`Importing issue`);
 
       // Import cover images sequentially
@@ -58,12 +84,12 @@ async function importIssue(data) {
       };
 
       const createIssue = await client.request(
-        withToken(BASE_ACCESS_TOKEN, createItems('issues', newData))
+        withToken(BASE_ACCESS_TOKEN, createItems("issues", newData))
       );
       return createIssue;
     }
   } catch (error) {
-    console.error('Error creating issue data:', error);
+    console.error("Error creating issue data:", error);
     console.error(error.extensions);
 
     // Handle the error and write specific data to a text file
@@ -71,7 +97,7 @@ async function importIssue(data) {
     const filePath = `sync/errors-issue.txt`;
 
     // Write the error data to the text file
-    fs.writeFileSync(filePath, failedData, 'utf-8');
+    fs.writeFileSync(filePath, failedData, "utf-8");
   }
 }
 
