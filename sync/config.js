94e7e7c29ae14114b3ddb6112ae51ed8ce6378ec
jeremyzilar <jeremyzilar@gmail.com>
2024-02-08 15:56:32 -0500

    use public endpoints

--- sync/config.js
+++ sync/config.js
@@ -3,9 +3,8 @@ module.exports = {
   // BASE_ACCESS_TOKEN: process.env.TOKEN_STAGING,
   // API_ENDPOINT: 'https://brooklynrail.org',
 
-  BASE_DIRECTUS_URL: 'http://127.0.0.1:8055',
+  BASE_DIRECTUS_URL: "http://127.0.0.1:8055",
   BASE_ACCESS_TOKEN: process.env.TOKEN_LOCAL,
-  API_ENDPOINT: 'http://localhost:8000',
-  
+  API_ENDPOINT: "https://brooklynrail.org",
+  // API_ENDPOINT: 'http://localhost:8000',
 };
-
