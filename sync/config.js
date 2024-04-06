44fbf30a1d7ff7b071c05ea2bed77e9b30a4e71f
jeremyzilar <jeremyzilar@gmail.com>
2024-04-06 07:48:09 -0400

    update paths

--- sync/config.js
+++ sync/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  // BASE_DIRECTUS_URL: 'https://brooklynrail-studio-staging-jy3zptd2sa-wl.a.run.app/',
-  // BASE_ACCESS_TOKEN: process.env.TOKEN_STAGING,
-  // API_ENDPOINT: 'https://brooklynrail.org',
+  // BASE_DIRECTUS_URL: "https://studio.brooklynrail.org",
+  // BASE_ACCESS_TOKEN: process.env.TOKEN,
 
   BASE_DIRECTUS_URL: "http://127.0.0.1:8055",
   BASE_ACCESS_TOKEN: process.env.TOKEN_LOCAL,
