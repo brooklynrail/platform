483bd41af2fb605e24f12cbd59eb689752ae8e08
jeremyzilar <jeremyzilar@gmail.com>
2024-04-15 14:15:14 -0400

    Revert "check to see if image already exists"
    This reverts commit ca18250363f48d4266a41e1d08c6fb51f683887a.

--- sync/importImageModule.js
+++ sync/importImageModule.js
@@ -12,60 +12,35 @@ const {
 async function importImageModule(data, issue_folder, client) {
   try {
     if (data && data.path) {
-      // Function to check if a image exists in Directus
-      const checkImageExists = async (path, client) => {
-        const image = await client.request(
-          withToken(
-            BASE_ACCESS_TOKEN,
-            readFiles({
-              query: {
-                filter: {
-                  old_path: {
-                    _eq: path,
-                  },
-                },
-              },
-            })
-          )
-        );
-        return image.length > 0 ? image[0].id : null;
-      };
+      // Import the image
+      // FULL path to Cloud Storage files
+      const path = `https://storage.googleapis.com/rail-legacy-media/production${data.path}`;
+      // Local, relative paths
+      // const path = `http://localhost:8000${data.path}`;
+      const description = data.description;
+      const caption = data.caption;
+      const shortcode_key = data.shortcode_key;
+      const old_path = data.old_path;
+      const tags = data.tags;
+      const folder = issue_folder.id;
 
-      const existingImageId = await checkImageExists(data.path, client);
-      if (existingImageId) {
-        console.log("Image already exists", existingImageId);
-        return existingImageId;
-      } else {
-        // Import the image
-        // FULL path to Cloud Storage files
-        const path = `https://storage.googleapis.com/rail-legacy-media/production${data.path}`;
-        // Local, relative paths
-        // const path = `http://localhost:8000${data.path}`;
-        const description = data.description;
-        const caption = data.caption;
-        const shortcode_key = data.shortcode_key;
-        const old_path = data.old_path;
-        const tags = data.tags;
-        const folder = issue_folder.id;
+      const result = await client.request(
+        withToken(
+          BASE_ACCESS_TOKEN,
+          importFile(path, {
+            description: description || null,
+            caption: caption || null,
+            shortcode_key: shortcode_key || null,
+            old_path: old_path || null,
+            tags: tags || null,
+            folder: folder || null,
+          })
+        )
+      );
 
-        const result = await client.request(
-          withToken(
-            BASE_ACCESS_TOKEN,
-            importFile(path, {
-              description: description || null,
-              caption: caption || null,
-              shortcode_key: shortcode_key || null,
-              old_path: old_path || null,
-              tags: tags || null,
-              folder: folder || null,
-            })
-          )
-        );
-
-        // return the ID of the file that was just uploaded
-        if (result) {
-          return result.id;
-        }
+      // return the ID of the file that was just uploaded
+      if (result) {
+        return result.id;
       }
     }
   } catch (error) {
@@ -80,6 +55,34 @@ async function importImageModule(data, issue_folder, client) {
   }
 }
 
+// check to see if image already exists in Directus
+// if it does, return the ID of the existing image
+async function checkForImage(data) {
+  console.log("checking for image data ----> ", data);
+  console.log("data.path", data.path);
+
+  const client = createDirectus(BASE_DIRECTUS_URL).with(rest());
+  const image = await client.request(
+    withToken(
+      BASE_ACCESS_TOKEN,
+      readFiles({
+        query: {
+          filter: {
+            old_path: {
+              _eq: data.path,
+            },
+          },
+        },
+      })
+    )
+  );
+
+  console.log("this is the image that was found ----> ", image);
+
+  return image.length > 0 ? image[0].id : null;
+}
+
 module.exports = {
   importImageModule,
+  checkForImage,
 };
