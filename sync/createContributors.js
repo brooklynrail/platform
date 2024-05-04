aaa5ba5fd7a7be175ff4db55f1f0a4dac7c3de12
jeremyzilar <jeremyzilar@gmail.com>
2024-05-04 13:39:21 -0400

    create one contributor at a time

--- sync/createContributors.js
+++ sync/createContributors.js
@@ -1,9 +1,22 @@
-require('dotenv').config();
-const { BASE_ACCESS_TOKEN, API_ENDPOINT, BASE_DIRECTUS_URL } = require('./config');
-const { createDirectus, rest, withToken, createItems} = require('@directus/sdk');
+require("dotenv").config();
+const fs = require("fs");
+const {
+  BASE_ACCESS_TOKEN,
+  API_ENDPOINT,
+  BASE_DIRECTUS_URL,
+} = require("./config");
+const {
+  createDirectus,
+  rest,
+  withToken,
+  createItems,
+  readItems,
+} = require("@directus/sdk");
 
 async function createContributors() {
   try {
+    const client = createDirectus(BASE_DIRECTUS_URL).with(rest());
+
     const response = await fetch(`${API_ENDPOINT}/api/contributors`, {
       headers: {
         Authorization: `Bearer ${BASE_ACCESS_TOKEN}`,
@@ -12,18 +25,64 @@ async function createContributors() {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    
-    const data = await response.json();
-    const client = createDirectus(BASE_DIRECTUS_URL).with(rest());
 
-    const contributors = await client.request(
-      withToken(BASE_ACCESS_TOKEN, createItems('contributors', data))
-    );
-    console.log(contributors);
+    const allContributors = await response.json();
 
-  } catch (error){
+    if (allContributors) {
+      for await (const contributorData of allContributors) {
+        // Check if contributorData.old_id exists in existingContributors
+        const existingContributors = await checkExistingContributors(
+          contributorData,
+          client
+        );
+
+        if (existingContributors.length > 0) {
+          console.log(
+            `Contributor ${contributorData.first_name} ${contributorData.last_name} already exists!`
+          );
+          continue;
+        }
+
+        // Create the contributor in Directus
+        const contributor = await client.request(
+          withToken(
+            BASE_ACCESS_TOKEN,
+            createItems("contributors", contributorData)
+          )
+        );
+        if (!contributor) {
+          throw new Error(`Error creating contributor: ${contributorData}`);
+        }
+        console.log(
+          `Contributor ${contributor.first_name} ${contributor.last_name} created!`
+        );
+      }
+    }
+  } catch (error) {
     console.error("Error creating contributors", error.message);
+    const failedData = `${error.message}\n`;
+    const filePath = `sync/errors-contributors.txt`;
+    fs.appendFileSync(filePath, failedData, "utf-8");
   }
+}
+
+async function checkExistingContributors(contributorData, client) {
+  const listContributors = await client.request(
+    withToken(
+      BASE_ACCESS_TOKEN,
+      readItems("contributors", {
+        fields: ["id", "old_id"],
+        filter: {
+          old_id: {
+            _eq: contributorData.old_id,
+          },
+        },
+        limit: -1,
+      })
+    )
+  );
+
+  return listContributors;
 }
 
 createContributors();
