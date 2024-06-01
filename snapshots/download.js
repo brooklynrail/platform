20061329042564f7e35a32e2656ea50b1b8e8adf
jeremyzilar <jeremyzilar@gmail.com>
2024-06-01 08:55:33 -0400

    add local path

--- snapshots/download.js
+++ snapshots/download.js
@@ -3,10 +3,14 @@ const fs = require("fs");
 require("dotenv").config();
 
 const token = process.env.TOKEN;
+const token_local = process.env.TOKEN_LOCAL;
+
 const url =
   "https://studio.brooklynrail.org/schema/snapshot?export=yaml&access_token=" +
   token;
-// console.log("token: ", token);
+// const url =
+//   "https://localhost:8055/schema/snapshot?export=yaml&access_token=" +
+//   token_local;
 
 https.get(url, (res) => {
   const path = "snapshots/init.yaml";
