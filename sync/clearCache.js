ff46f30c86bcd319f2b8ff92c305741b60425ecb
jeremyzilar <jeremyzilar@gmail.com>
2024-08-20 17:22:12 -0600

    clear Directus Cache function

---
+++ sync/clearCache.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { BASE_ACCESS_TOKEN, BASE_DIRECTUS_URL } = require("./config");
+const {
+  createDirectus,
+  rest,
+  clearCache,
+  withToken,
+} = require("@directus/sdk");
+
+// Create all the articles for each issue
+async function clear() {
+  try {
+    const client = createDirectus(BASE_DIRECTUS_URL).with(rest());
+    const result = await client.request(
+      withToken(BASE_ACCESS_TOKEN, clearCache())
+    );
+    console.log("Cache cleared! ", result);
+  } catch (error) {
+    console.error("Error clearing cache: ", error.message);
+  }
+}
+clear();
