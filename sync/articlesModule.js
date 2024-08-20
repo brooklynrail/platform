5012b78758a0833411925d35acb076fe36458103
jeremyzilar <jeremyzilar@gmail.com>
2024-08-20 17:21:46 -0600

    rm articles_slug

--- sync/articlesModule.js
+++ sync/articlesModule.js
@@ -16,95 +16,90 @@ async function importArticles(
 ) {
   try {
     // Check to see if the article already exists in Directus
-    const old_id = articleData.articles_slug.old_id;
+    const old_id = articleData.old_id;
 
     const checkArticleExists = async (old_id, client) => {
       const articles = await client.request(
         withToken(
           BASE_ACCESS_TOKEN,
           readItems("articles", {
-            filter: {
-              old_id: {
-                _eq: old_id,
-              },
-            },
+            filter: { old_id: { _eq: old_id } },
           })
         )
       );
+
       return articles.length > 0 ? true : false;
     };
 
     const articleExists = await checkArticleExists(old_id, client);
     if (articleExists) {
-      console.log(
-        `Article ${articleData.articles_slug.title} already exists! Skipping...`
-      );
+      // console.log(`Article ${articleData.title} already exists! Skipping...`);
       return;
     }
 
-    console.log("====================================");
-    console.log("Importing article data: ", articleData.articles_slug.title);
-    console.log("\n");
+    console.log("=====");
+    console.log(existingIssue.title);
+    console.log(`#${articleData.sort} | ${articleData.title}`);
 
-    const section = await sectionsModule(
-      articleData.articles_slug.old_section_id,
-      client
-    );
+    const section = await sectionsModule(articleData.old_section_id, client);
 
     const contributors = await contributorsModule(
-      articleData.articles_slug.contributors,
+      articleData.contributors,
       client
     );
 
     const issue = { id: existingIssue.id };
 
     let images;
-    if (articleData.articles_slug.images !== null) {
+    if (articleData.images !== null) {
       images = await articleImagesModule(
-        articleData.articles_slug.images,
+        articleData.images,
         articles_folder,
         client
       );
     }
 
     const promo_banner = await importImageModule(
-      articleData.articles_slug.promo_banner,
+      articleData.promo_banner,
       articles_folder,
       client
     );
 
     const promo_thumb = await importImageModule(
-      articleData.articles_slug.promo_thumb,
+      articleData.promo_thumb,
       articles_folder,
       client
     );
 
     const slideshow_image = await importImageModule(
-      articleData.articles_slug.slideshow_image,
+      articleData.slideshow_image,
       articles_folder,
       client
     );
 
     let featured_image;
-    if (articleData.articles_slug.featured_image !== null) {
+    if (articleData.featured_image !== null) {
       featured_image = await importImageModule(
-        articleData.articles_slug.featured_image,
+        articleData.featured_image,
         articles_folder,
         client
       );
     }
 
     // check if there are wrapping <p> tags in the excerpt and remove them
-    let excerpt = articleData.articles_slug.excerpt;
+    let excerpt = articleData.excerpt;
     if (excerpt) {
       excerpt = excerpt.replace(/<p>/g, "").replace(/<\/p>/g, "");
     }
 
     // add the excerpt to the article object
-    articleData.articles_slug.excerpt = excerpt;
+    articleData.excerpt = excerpt;
+
+    // remove the body_code from the articleData.articles_slug object
+    delete articleData.body_code;
 
     const newData = {
-      ...articleData.articles_slug,
+      ...articleData,
       section,
       contributors,
       featured_image,
@@ -115,24 +110,20 @@ async function importArticles(
       issue,
     };
 
-    console.log("newData ===========================", newData);
-
-    // console.log("New article data: ", newData);
-
     // // Create the new article in Directus
     const newarticle = await client.request(
       withToken(BASE_ACCESS_TOKEN, createItem("articles", newData))
     );
 
-    console.log("Article import completed!", newarticle);
+    // console.log("Article import completed!", newarticle);
   } catch (error) {
     console.error(
-      `Error importing article data for ${articleData.articles_slug.title}:`,
-      error.message
+      `Error importing article data for ${articleData.title}:`,
+      error
     );
 
     // Handle the error and write specific data to a text file
-    const failedData = `${existingIssue.title} | ${articleData.articles_slug.title}\n`;
+    const failedData = `${existingIssue.title} | ${articleData.title}\n`;
     const filePath = `sync/errors-articles.txt`;
 
     // Write the error data to the text file
