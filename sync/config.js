33e274a79154e717a462d6f02e9f9f4df5587c9d
jeremyzilar <jeremyzilar@gmail.com>
2024-09-23 07:33:39 -0400

    use local

--- sync/config.js
+++ sync/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  BASE_DIRECTUS_URL: "https://studio.brooklynrail.org",
-  BASE_ACCESS_TOKEN: process.env.TOKEN,
+  // BASE_DIRECTUS_URL: "https://studio.brooklynrail.org",
+  // BASE_ACCESS_TOKEN: process.env.TOKEN,
 
-  // BASE_DIRECTUS_URL: "http://127.0.0.1:8055",
-  // BASE_ACCESS_TOKEN: process.env.TOKEN_LOCAL,
+  BASE_DIRECTUS_URL: "http://127.0.0.1:8055",
+  BASE_ACCESS_TOKEN: process.env.TOKEN_LOCAL,
 
   API_ENDPOINT: "https://brooklynrail.org",
   // API_ENDPOINT: "http://localhost:8000",
